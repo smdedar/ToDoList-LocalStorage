39301c6f93b2c8113498b5fbf4340b0fd263bcb4
smdedar <thededar@gmail.com>
2020-01-18 23:58:10 +0600

    design change

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-function saveData(){
+function saveData() {
     var data = document.getElementById("task").value;
     document.getElementById("task").value = null;
 
     var task = [];
     var storageCheck = JSON.parse(localStorage.getItem("todoTask"));
 
-    if(storageCheck != null){
+    if (storageCheck != null) {
         task = JSON.parse(localStorage.getItem('todoTask'));
     }
-    
+
     task.push(data);
-    alert(task);
-    localStorage.setItem('todoTask',JSON.stringify(task));
+    //alert(task);
+    localStorage.setItem('todoTask', JSON.stringify(task));
 
     viewData();
 }
 
 
-function viewData(){
+function viewData() {
     var storageCheck = JSON.parse(localStorage.getItem("todoTask"));
     //console.log(storageCheck);
-   var  li = '';
-    for(var i=0; i<storageCheck.length; i++){
-        li += '<li>'+storageCheck[i]+'<button onclick="deleteData('+i+')">X</button></li>';
+    var li = '';
+    for (var i = 0; i < storageCheck.length; i++) {
+        li += '<li>' + storageCheck[i] + '<button id="deleteBtn" onclick="deleteData(' + i + ')">X</button></li>';
     }
     document.getElementById("viewTask").innerHTML = li;
 }
 
 
-function deleteData(index){
+function deleteData(index) {
     var task = JSON.parse(localStorage.getItem('todoTask'));
-    task.splice(index,1);
+    task.splice(index, 1);
     alert("Delete");
     localStorage.setItem('todoTask', JSON.stringify(task));
 
     viewData();
 }
 
-window.onload = function() {
+window.onload = function () {
     viewData();
-  };
+};
